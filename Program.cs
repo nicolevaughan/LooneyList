a96f5f8a3647be7f6c3085692f80a84fc4823b8e
nicol <nicol@BLUE_LAPTOP>
2024-03-16 23:32:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace LooneyList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> members = new List<string> { "Bugs", "Daffy", "Porky", "Lola", "Speedy" };
+
+            string[] memberArr = new string[] { "Tweety", "Sylvester", "Tina" };
+            members.AddRange(memberArr);
+
+            members.Sort();
+            Console.WriteLine("-------Ascending--------");
+            foreach (string m in members)
+                Console.WriteLine(m);
+
+            members.Remove("Sylvester");
+
+            members.Reverse();
+            Console.WriteLine("-------Descending--------");
+            foreach (string m in members)
+                Console.WriteLine(m);
+
+
+        }
+    }
+}
